174005788bd0d3df997f9057b9e54719a10b6ba8
oisaqueandrade <90810299+oisaque@users.noreply.github.com>
2022-08-24 21:03:45 -0300

    Tarefa 02 de Linguagem de Programação Java

---
+++ Tarefa02/Exercicio01.java
@@ -0,0 +1,27 @@
+//Comentario InLine - Em Linha
+
+/* Coment�rio MultiLinha
+1) Solicite um valor do teclado e exiba seu sucessor. */
+
+/**
+ * @author aluno
+ */
+
+package exercicio;
+
+import java.util.Scanner; // A Classe Scanner lê o que for digitado pelo teclado
+
+public class Exercicio01 {
+	
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite um valor: ");
+		int numero=sc.nextInt();
+		int sucessor = (numero+1);
+		
+		System.out.println("O sucessor do número " +numero+ " é o número: " +sucessor);
+sc.close();
+	}
+
+}
